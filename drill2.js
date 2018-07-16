ee0ec90d014db336d1be7b58a1f3481aa2ece7b3
kathyleboeuf <kleboeuf@townisp.com>
2018-07-16 16:56:18 -0400

    headache with code

--- drill2.js
+++ drill2.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function jediName(firstName, lastName) {
 	return lastName.slice(0, 3) + firstName.slice(0, 2);
 }
@@ -21,3 +23,32 @@ function beyond(num) {
 console.log(beyond(Infinity));
 console.log(beyond(-5));
 console.log(beyond(0));
+
+/* ==================================== */
+function decode(string) {
+
+	switch (string[0]) {
+	case "a" :
+		return string[1];
+
+	case "b" :
+		return string[2];
+
+	case "c" :
+		return string[3];
+
+	case "d" :
+		return string[4];
+
+	default :
+		return " ";
+	}
+}
+console.log(decode("craft"));
+console.log(decode("block"));
+console.log(decode("argon"));
+console.log(decode("meter "));
+console.log(decode("bells "));
+console.log(decode("brown "));
+console.log(decode("croon "));
+console.log(decode("droop "));
