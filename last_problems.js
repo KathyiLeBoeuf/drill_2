221eb34b24ca3212bb7440540b70464cc4928d22
kathyleboeuf <kleboeuf@townisp.com>
2018-07-16 17:46:23 -0400

    rock on

--- last_problems.js
+++ last_problems.js
@@ -14,7 +14,7 @@ function daysOfMonths(month, leapYear){
                     return `${month} has 29 days.`
                 default :
                     return `${month} has 28 days.`
-            } 
+            }
         case 'April' :
         case 'June' :
         case 'September' :
@@ -29,3 +29,31 @@ console.log(daysOfMonths('January'));
 console.log(daysOfMonths('February'));
 console.log(daysOfMonths('February', true));
 console.log(daysOfMonths('Kathy'));
+
+
+
+
+
+function rockPaperScissors(num) {
+    let computer = Math.floor(Math.random() * 3) + 1;
+
+    if (computer ===  num) {
+        return "It's a tie"
+    } if (computer === 1 && num !== 2) {
+        return "Computer Rock beats Scissors"
+    } if (computer === 2 && num !== 3) {
+        return "Computer Paper beats Rock"
+    } if(computer === 3 && num !== 1) {
+        return "Computer Scisors beats Paper"
+    } if (num === 1 && computer !== 2) {
+        return "Computer Rock beats Scissors"
+    } if (num === 2 && computer !== 3) {
+        return "Computer Paper beats Rock"
+    } if(num === 3 && computer !== 1) {
+        return "Computer Scisors beats Paper"
+}
+;
+console.log(rockPaperScissors(3));
+console.log(rockPaperScissors(3));
+console.log(rockPaperScissors(3));
+console.log(rockPaperScissors(3));
